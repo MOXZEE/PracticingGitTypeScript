4a2634db319232e1598e72ac945ac5e49ad3a23f
MOXZEE <zemcdee@gmail.com>
2022-03-25 15:55:06 -0700

    Fix es6 shims errors

---
+++ public/GettingStarted.js
@@ -0,0 +1,19 @@
+class App {
+}
+App.twoSum = (nums, target) => {
+    let counter = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const current = target - nums[i];
+        if (!counter.has(current)) {
+            counter[current] = i;
+        }
+        else {
+            return [counter[current], i];
+        }
+    }
+    return [12];
+};
+let nums = new Array(1, 4, 7, 3, 7, 5);
+let target = 7;
+console.log(App.twoSum(nums, target));
+//# sourceMappingURL=GettingStarted.js.map
